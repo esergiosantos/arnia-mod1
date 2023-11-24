551779bb1481740be06e0b74c23667244fc9093a
esergiosantos <esergiosantos1@gmail.com>
2023-11-23 22:59:04 -0300

    Aula17

---
+++ aula17/exemplo1/app.js
@@ -0,0 +1,50 @@
+//Váriavel recebendo texto
+let frase = "Alô alunos da Turma 7";
+frase = "Arnia Escola de Programação";
+console.log(frase);
+const frase2 = "teste";
+
+//Váriavel recebendo numero
+let numero = 1;
+console.log(numero);
+
+let numeroDecimal = 1.5;
+console.log(numeroDecimal);
+
+//somar 2 numeros
+let soma = 2 + 2;
+console.log(soma);
+
+let subtracao = 10 - 5;
+console.log(subtracao);
+
+//multiplicação
+let multiplicacao = 2 * 5;
+console.log(multiplicacao);
+
+//divisão
+let divisao = 30 / 2;
+console.log(divisao);
+
+//potenciação (5  elevado á 2)
+let potenciacao = 5 ** 2;
+console.log(potenciacao);
+
+//resto de uma divisão
+let resto = 9 % 2;
+console.log(resto);
+
+let soma3 = 3 + 8 + 9;
+console.log(soma3);
+
+let somaTiposDiferentes = 3 * "Arnia";
+console.log(somaTiposDiferentes);
+
+let somaNumeros = "1" + "1";
+console.log(somaNumeros);
+
+let soma4 = 2 + "2";
+console.log(soma4);
+
+let operação = 2 + ((3 * 4) / 8) * (4 + 5);
+console.log(operação);               
---
+++ aula17/exercicio01/app.js
@@ -0,0 +1,41 @@
+//Faça um programa de computador para escrever seus dados pessoais
+
+let nome = "Sergio "
+let idade = "30anos "
+let peso = "85KG "
+let nomeIdadePeso = nome + idade + peso;
+console.log(nomeIdadePeso);
+
+
+//Faça um programa de computador para escrever a letra de uma música em inglês que você goste muito
+let letra = "My only peace of mind\nPlease, tell me\nIt's by your side\n\nDo you know some place, where I can hide\nFrom all of the highs and all of the lows?\nThe place where I will find, my only peace of mind\nPlease, tell me it's by your side\n\nMy only peace of mind\nPlease, tell me\n\nDo you know some place, where I can hide\nFrom all of the highs and all of the lows?\nThe place where I will find, my only peace of mind\nPlease, tell me it's by your side\n\nMy only peace of mind\nPlease, tell me\nIt's by your side\n\nMy only peace of mind\nPlease, tell me\nIt's by your side";
+console.log(letra);
+
+
+//Faça um programa que tenha a seguinte instrução x = 2 + 3 * 4 e depois
+
+let x = 2 + 3 * 4;
+console.log(x);
+
+//Faça um programa de computador para escrever o resultado da expressão 10 - “2”. Analise o resultado
+
+let expressão = 10 - "2";
+console.log(expressão);
+
+//Faça um programa que escreva a raiz cúbica de 13
+
+let raiz = Math.sqrt(13);
+console.log(raiz);
+
+//Faça um programa que escreva o resultado da expressão 99 * 1.01^12, o valor de c deverá ser escolhido pelo programador (o ^ indica “elevado”, no JS use **)
+
+let c = 99
+let resultado = c * 1.01**12;
+console.log(resultado);
+
+
+
+
+
+
+
